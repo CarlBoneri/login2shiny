fb0b67f34a0de79231fd15a5f8b4ec1a67f447e5
CarlBoneri <carl.boneri@whyles.com>
2020-09-20 15:24:40 -0400

    move sh?

--- git_init_login2shiny.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "# login2shiny" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/CarlBoneri/login2shiny.git
-git push -u origin master
